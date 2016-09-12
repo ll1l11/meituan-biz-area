ae77e643c0f7bc359918d58422320a2294161b34
ll1l11 <ll1l11@qq.com>
2016-09-12 17:07:56 +0800

    可以获取城市列表

---
+++ city.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""通过网页版的美团获取城市列表
+将结果保存到cities.json
+"""
+import re
+import json
+import string
+import requests
+from lxml import etree
+
+_CITY_URL_PATTERN = re.compile('http://(\w+)\.')
+
+
+def get_changecity_html():
+    # 网页版城市列表
+    url = 'http://www.meituan.com/index/changecity/initiative'
+    return requests.get(url).text
+
+def get_pinyin_by_url(url):
+    m = _CITY_URL_PATTERN.match(url)
+    if m:
+        return m.group(1)
+
+
+
+def parse_city(html, letter):
+    xpath = '//*[@id="city-{0}"]'.format(letter)
+    result = html.xpath(xpath)
+    if not result:
+        return
+    # print(result[0])
+    cities = []
+    for a in result[0].findall('.//a'):
+        name = a.text
+        url = a.attrib['href']
+        pinyin = get_pinyin_by_url(url)
+        city = dict(
+            name=name,
+            pinyin=pinyin
+        )
+        cities.append(city)
+    return cities
+
+
+def save(items):
+    content = json.dumps(items)
+    with open('cities.json', 'w') as f:
+        f.write(content)
+
+
+def main():
+    html = etree.HTML(get_changecity_html())
+    # xpath: //*[@id="city-B"]
+    items = []
+    for letter in string.ascii_uppercase:
+        cities = parse_city(html, letter)
+        item = dict(
+            letter=letter,
+            cities=cities
+        )
+        items.append(item)
+    save(items)
+
+
+if __name__ == '__main__':
+    main()
